6468cb4a4d99e0cd4a2715283cdb7aa3407d6dfe
Jack Valley <Juuso.laakso@turku.fi>
2021-07-28 14:59:05 +0300

    Revert "translations"
    This reverts commit 39ff11aec0a58f51d4d3bad064786e77529dfda3.

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -2,22 +2,15 @@ import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
 import Työpiste from '../../public/tyopiste_logo.webp'
 
-import en from './en'
-import fi from './fi'
-import { useRouter } from 'next/dist/client/router'
-
 export default function About() {
-
-  const router = useRouter();
-  const { locale } = router;
-  const trans = locale === 'fi' ? fi : en;
-
   return (
     <div className={styles.about}>
       <div className={styles.abouttext}>
         <h1 className={styles.title}>About us</h1>
         <p>
-          {trans.about}
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus blanditiis odit
+          necessitatibus, rerum voluptates modi recusandae maiores doloribus. Itaque, ex!
+          Aspernatur, recusandae voluptate. Quas similique, vero neque deserunt officiis in!
         </p>
       </div>
       <a href="https://www.turku.fi/tyopiste" rel="noreferrer" target="_blank">

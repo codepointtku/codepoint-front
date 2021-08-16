bad727b3b2de480a6962071e87ff088e01208fa2
Ninja Kanerva <ninja.kanerva@gmail.com>
2021-08-16 13:02:25 +0300

    Spaces removed, router removed, quotes changed
    Whitespaces removed, router removed (unused), quotes changed to double quotes

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,42 +2,30 @@ import Image from 'next/image'
 import styles from '../../styles/Home.module.scss'
 import Link from 'next/link'
 import SetLanguage from '../set-language/SetLanguage'
-import { useRouter } from 'next/router'
 
 export default function Header() {
-
-  const router = useRouter()
-  
   return (
-    <header className={styles.header} id='about'>
+    <header className={styles.header} id="about">
       <Link href="/" passHref>
         <a>
-          <Image
-            src="/codepoint_logo_lapinakyva2.svg"
-            alt="CodePoint logo"
-            width={160}
-            height={23}
-            layout="intrinsic"
-            quality={60}
-            priority
-          />
+          <Image src="/codepoint_logo_lapinakyva2.svg" alt="CodePoint logo" width={160} height={23} layout="intrinsic" quality={60} priority/>
         </a>
       </Link>
       <SetLanguage />
       
       <ul className={styles.menu}>
         <li className={styles.menu_item}>
-          <a href='#about'>
+          <a href="#about">
             About Us
           </a>
         </li>
         <li className={styles.menu_item}>
-          <a href='#projects' id='projects-text'>
+          <a href="#projects" id='projects-text'>
             Projects
           </a>
         </li>
         <li className={styles.menu_item}>
-          <a href='#members' id='members-text'>
+          <a href="#members" id="members-text">
             Members
           </a>
         </li>
@@ -45,12 +33,7 @@ export default function Header() {
       
       <div className={styles.btn_container}>
         <ul className={styles.dropdown}>
-          <Image 
-            src="/icons8-language-60-white.png"
-            alt="Language selection"
-            width={25}
-            height={25}
-          />
+          <Image src="/icons8-language-60-white.png" alt="Language selection" width={25} height={25}/>
           <li className={styles.language}><a href="#">English</a></li>
           <li className={styles.language}><a href="#">Suomi</a></li>
         </ul>

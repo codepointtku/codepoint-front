9949c4e2d910e5f92d77e45cf183b7704cb8e833
juhath <juha.heikkila@hotmail.fi>
2021-08-19 10:11:47 +0300

    Additional stylings
    Added additional stylings to the Header and Nav components

--- components/nav/Nav.tsx
+++ components/nav/Nav.tsx
@@ -1,26 +1,54 @@
+import {useTranslation} from 'next-i18next'
+import {useRouter} from 'next/dist/client/router'
 import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../../styles/Home.module.scss'
 
 export default function Nav() {
+  const router = useRouter()
+  const {t} = useTranslation(['common'])
   return (
     <nav className={styles.nav}>
       <Link href="/">
-        <a className={styles.tabBtn} id="about_image">
-          <Image src="/info-solid.svg" alt="About us" id="about_icon" width={30} height={30} />
-          <p>About Us</p>
+        <a
+          className={router.pathname === '/' ? styles.tabBtn_selected : styles.tabBtn}
+          id="about_image">
+          <Image
+            src={router.pathname === '/' ? '/info-solid-orange.svg' : '/info-solid.svg'}
+            alt="About us"
+            id="about_icon"
+            width={30}
+            height={30}
+          />
+          <p>{t('aboutus')}</p>
         </a>
       </Link>
       <Link href="/projects">
-        <a className={styles.tabBtn} id="projects_image">
-          <Image src="/project.svg" alt="Projects" id="projects-icon" width={30} height={30} />
-          <p>Projects</p>
+        <a
+          className={router.pathname === '/projects' ? styles.tabBtn_selected : styles.tabBtn}
+          id="projects_image">
+          <Image
+            src={router.pathname === '/projects' ? '/project-orange.svg' : '/project.svg'}
+            alt="Projects"
+            id="projects-icon"
+            width={30}
+            height={30}
+          />
+          <p>{t('projects')}</p>
         </a>
       </Link>
       <Link href="/members">
-        <a className={styles.tabBtn} id="members_image">
-          <Image src="/users-solid.svg" alt="Members" id="members-icon" width={30} height={30} />
-          <p className={styles.nav_text}>Members</p>
+        <a
+          className={router.pathname === '/members' ? styles.tabBtn_selected : styles.tabBtn}
+          id="members_image">
+          <Image
+            src={router.pathname === '/members' ? '/users-solid-orange.svg' : '/users-solid.svg'}
+            alt="Members"
+            id="members-icon"
+            width={30}
+            height={30}
+          />
+          <p>{t('members')}</p>
         </a>
       </Link>
     </nav>

7056123582217fdc2f83d1577785c80bb6f1018d
juhath <juha.heikkila@hotmail.fi>
2021-08-19 12:24:30 +0300

    Removed unused queries and fixed one test

--- pages/members.tsx
+++ pages/members.tsx
@@ -1,20 +1,15 @@
-// import styles from '../styles/Home.module.scss'
 import Profiles from '../components/profiles/Profiles'
 import dynamic from 'next/dynamic'
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations'
 import Head from 'next/head'
-import {gql} from '@apollo/client'
-import client from '../apollo-client'
 import Header from '../components/header/Header'
 import Nav from '../components/nav/Nav'
-import {InferGetServerSidePropsType} from 'next'
 
 const DynamicFooter = dynamic(() => import('../components/footer/Footer'))
 
-export default function Members({team}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+export default function Members() {
   return (
     <>
-      {/* <div className={styles.container}> */}
       <Head>
         <title>CodepointTKU Members</title>
         <meta
@@ -24,58 +19,13 @@ export default function Members({team}: InferGetServerSidePropsType<typeof getSe
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      {/* <main className={styles.main}> */}
-      <Profiles team={team} />
-      {/* </main> */}
+      <Profiles />
       <DynamicFooter />
       <Nav />
-      {/* </div> */}
     </>
   )
 }
 
 export async function getServerSideProps({locale}: any) {
-  const {data} = await client.query({
-    query: gql`
-      query Codepoint {
-        organization(login: "codepointtku") {
-          name
-          description
-          repositories(first: 6, orderBy: {field: PUSHED_AT, direction: DESC}) {
-            edges {
-              node {
-                id
-                homepageUrl
-                description
-                name
-                url
-              }
-            }
-          }
-          team(slug: "Developers") {
-            members(orderBy: {field: LOGIN, direction: ASC}) {
-              edges {
-                node {
-                  id
-                  name
-                  bio
-                  url
-                  avatarUrl(size: 512)
-                }
-              }
-            }
-          }
-        }
-      }
-    `,
-  })
-
-  return {
-    props: {
-      org: data.organization,
-      repos: data.organization.repositories.edges,
-      team: data.organization.team.members.edges,
-      ...(await serverSideTranslations(locale, ['common', 'footer'])),
-    },
-  }
+  return {props: {...(await serverSideTranslations(locale, ['common', 'footer']))}}
 }

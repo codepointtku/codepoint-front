5d4a8cc0f98b25399e041c5d31e3db9bf6d3e1ad
Ville Säkkinen <sakkisen.ville@gmail.com>
2021-09-02 11:46:59 +0300

    simplified functions

--- pages/api/locales/ko/footer.js
+++ pages/api/locales/ko/footer.js
@@ -1,18 +1,13 @@
 import {TranslateFooter} from '../../translator/translator'
 
-
 export default async function Footer(req, res){
-  const {city, post, phone, accessibility, report} = await TranslateFooter('ko')
   return (
     res.statusCode = 200,
     res.setHeader('Content-Type', 'application/json'),
     res.end(
-      JSON.stringify({
-      city: city,
-      post: post,
-      phone: phone,
-      accessibility: accessibility,
-      report: report})
+      JSON.stringify(
+        await TranslateFooter('ko')
+      )
     )
   )
 }
